ad873ca6189de8e3d67056526e6043f5a645e8ad
Kounex <r.schramowski@googlemail.com>
2025-05-17 23:03:15 +0200

    chore(custom category): changed layout of the view

--- lib/views/tabs/settings/custom_categories/custom_categories.dart
+++ lib/views/tabs/settings/custom_categories/custom_categories.dart
@@ -22,65 +22,72 @@ class _CustomCategoriesViewState extends State<CustomCategoriesView> {
 
   @override
   Widget build(BuildContext context) {
-    return ImpostiScaffold(
-      includeBackButton: true,
-      body: HiveBuilder<Category>(
-        hiveKey: HiveKey.category,
-        builder: (context, categoryBox, child) {
-          Iterable<Category> customCategories = categoryBox.values.where(
-            (category) => !category.base,
-          );
-          return BaseCard(
-            topPadding: 0,
-            leftPadding: 0,
-            rightPadding: 0,
-            bottomPadding: 0,
-            paddingChild: EdgeInsets.zero,
-            title: 'settingsYourCategories'.tr(),
-            trailingTitleWidget: CupertinoButton(
-              onPressed: () => CustomCategoryService.addEditSheet(context),
-              padding: EdgeInsets.zero,
-              minSize: DesignSystem.size.x18,
-              child: Text('gAdd'.tr()),
-            ),
-            child: switch (customCategories.isNotEmpty) {
-              true => ConstrainedBox(
-                constraints: BoxConstraints(maxHeight: DesignSystem.size.x256),
-                child: Scrollbar(
-                  controller: _controller,
-                  thumbVisibility: true,
-                  child: ListView(
+    return HiveBuilder<Category>(
+      hiveKey: HiveKey.category,
+      builder: (context, categoryBox, child) {
+        Iterable<Category> customCategories = categoryBox.values.where(
+          (category) => !category.base,
+        );
+        return ImpostiScaffold(
+          includeBackButton: true,
+          children: [
+            SizedBox(height: DesignSystem.spacing.x128),
+            BaseCard(
+              topPadding: 0,
+              leftPadding: 0,
+              rightPadding: 0,
+              bottomPadding: 0,
+              paddingChild: EdgeInsets.zero,
+              title: 'settingsYourCategories'.tr(),
+              trailingTitleWidget: CupertinoButton(
+                onPressed: () => CustomCategoryService.addEditSheet(context),
+                padding: EdgeInsets.zero,
+                minSize: DesignSystem.size.x18,
+                child: Text('gAdd'.tr()),
+              ),
+              child: switch (customCategories.isNotEmpty) {
+                true => ConstrainedBox(
+                  constraints: BoxConstraints(
+                    maxHeight: DesignSystem.size.x256,
+                  ),
+                  child: Scrollbar(
                     controller: _controller,
-                    shrinkWrap: true,
-                    children: List.from(
-                      customCategories.map(
-                        (category) => ListTile(
-                          onTap:
-                              () => CustomCategoryService.addEditSheet(
-                                context,
-                                category,
+                    thumbVisibility: true,
+                    child: ListView(
+                      controller: _controller,
+                      shrinkWrap: true,
+                      children: List.from(
+                        customCategories.map(
+                          (category) => ListTile(
+                            onTap:
+                                () => CustomCategoryService.addEditSheet(
+                                  context,
+                                  category,
+                                ),
+                            title: Padding(
+                              padding: EdgeInsets.only(
+                                left: DesignSystem.spacing.x8,
                               ),
-                          title: Padding(
-                            padding: EdgeInsets.only(
-                              left: DesignSystem.spacing.x8,
+                              child: Text(category.name['custom']!),
                             ),
-                            child: Text(category.name['custom']!),
+                            trailing: Icon(Icons.edit),
                           ),
-                          trailing: Icon(Icons.edit),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              false => Padding(
-                padding: EdgeInsets.all(DesignSystem.spacing.x24),
-                child: BasePlaceholder(text: 'lobbyCategoryListOwnEmpty'.tr()),
-              ),
-            },
-          );
-        },
-      ),
+                false => Padding(
+                  padding: EdgeInsets.all(DesignSystem.spacing.x24),
+                  child: BasePlaceholder(
+                    text: 'lobbyCategoryListOwnEmpty'.tr(),
+                  ),
+                ),
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
